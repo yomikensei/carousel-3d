6f14d06d4a65c421a0423790378aa25f96ef6f3c
devMarshall <onyendo52@gmail.com>
2019-06-10 16:37:46 +0100

    chore: remove tabnavigator

--- App/index.js
+++ App/index.js
@@ -7,51 +7,25 @@
  */
 
 import React from 'react'
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import { Platform } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 import Screen1 from './screens/screen1';
 import Screen2 from './screens/screen2';
 
-const App = createBottomTabNavigator(
+const App = createStackNavigator(
   {
     Screen1: {
       screen: Screen1,
       navigationOptions: () => ({
-        tabBarIcon: ({ tintColor }) => (
-          <Icon name="list-alt" size={18} color={tintColor} />
-        ),
+        header: null,
       }),
     },
     Screen2: {
       screen: Screen2,
       navigationOptions: () => ({
-        tabBarIcon: ({ tintColor }) => (
-          <Icon name="users" size={18} color={tintColor} />
-        ),
+        header: null,
       }),
     },
   },
-  {
-    tabBarOptions: {
-      showLabel: false,
-      activeTintColor: '#586589',
-      backBehavior: 'initialRoute',
-      tabStyle: {
-        margin: 0,
-        padding: 0
-      },
-      style: {
-        backgroundColor: 'white',
-        // marginBottom: 20,
-        borderWidth: 0,
-        // width: '80%',
-        alignSelf: 'center',
-        margin: 0,
-        padding: 0,
-      },
-    }
-  }
 );
 
 
